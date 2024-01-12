31abb83d13f961f55162a7312d6b09749479057f
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2024-01-12 17:07:51 +0100

    Add Timer Resource in HelloPlugin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::{App, Startup, Update, Query, DefaultPlugins, Plugin};
 use bevy::ecs::component::Component;
-use bevy::ecs::system::Commands;
+use bevy::ecs::system::{Commands, Resource, Res, ResMut};
 use bevy::ecs::query::With;
+use bevy::time::{Time, Timer, TimerMode};
 
 fn main() {
     App::new()
@@ -34,14 +35,21 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
             .add_systems(Update, greet_people);
     }
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update the timer with the time elapsed since the last update
+    // if that caused the timer to finish, say hello
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
-
